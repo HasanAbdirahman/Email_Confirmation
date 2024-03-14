0baaa1f363b6dc5bd3238dc056ce1d3cf10d95d8
HasanAbdirahman <108181341+HasanAbdirahman@users.noreply.github.com>
2024-03-14 06:24:59 +0000

    almost done

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,17 +1,31 @@
 import { useState } from 'react'
+import { Resend } from 'resend';
+import { Email } from './Email';
 
 export default function Input(){
     const [email, setEmail] = useState('');
     const [checked, setChecked]= useState(false)
+    
+    const resend = new Resend(process.env.API_KEY);
 
+   async function  handleSubmit(e){
+        e.preventDefault();            
+        await resend.emails.send({
+            from: 'hasanabdirahman1999@gmail.com',
+            to: email,
+            subject: 'hello world',
+            react: <Email url="https://example.com" />,
+        });
+    }
     function handleChange(e){
         setEmail(e.target.value)
     }
     function handleChecked(e){
         setChecked(!checked)
     }
+
     return(
-        <form >
+        <form onSubmit={handleSubmit}>
             <input style={{padding: '0.5rem', borderRadius: '10px', borderColor: 'black'}}type="email" id="email" placeholder="Fill in your email" value={email} onChange={handleChange}/>
             <div>
             <div> Subcribe to the newsletter for marketing.
@@ -25,3 +39,13 @@ export default function Input(){
         </form>
     )
 }
+
+
+const resend = new Resend('re_123456789');
+
+await resend.emails.send({
+  from: 'you@example.com',
+  to: 'user@gmail.com',
+  subject: 'hello world',
+  react: <Email url="https://example.com" />,
+});
