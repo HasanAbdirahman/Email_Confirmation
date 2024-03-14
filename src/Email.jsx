2018370982c355b1cbc0a86e132019418a60d8b9
HasanAbdirahman <108181341+HasanAbdirahman@users.noreply.github.com>
2024-03-14 08:15:24 +0000

    done

--- src/Email.jsx
+++ src/Email.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import  React from 'react';
 import { Html } from '@react-email/html';
 import { Button } from '@react-email/button';
 
--- src/Input.jsx
+++ src/Input.jsx
@@ -6,17 +6,23 @@ export default function Input(){
     const [email, setEmail] = useState('');
     const [checked, setChecked]= useState(false)
     
-    const resend = new Resend(process.env.REACT_APP_API_KEY);
-
-   async function  handleSubmit(e){
+    
+    async function  handleSubmit(e){
         e.preventDefault();     
         if (checked && email){
-            await resend.emails.send({
-                from: 'hasanabdirahman1999@gmail.com',
-                to: email,
-                subject: 'hello world',
-                react: <Email url="https://example.com" />,
-            });
+            try {
+                const resend = new Resend("re_Jfj7GyeA_L9kY8pzfZ8drMLFcsGp54dsW");
+                await resend.emails.send({
+                    from: 'Hasan <hasanabdirahman1999@gmail.com>',
+                    to: [email],
+                    subject: 'Hello World',
+                    // react: <Email url="https://example.com" />,
+                    html: '<strong>It works!</strong>'
+                });
+                console.log('Email sent successfully');
+            } catch (error) {
+                console.error('Error sending email:', error);
+            }
         }       
     }
     function handleChange(e){
@@ -30,7 +36,7 @@ export default function Input(){
         <form onSubmit={handleSubmit}>
             <input style={{padding: '0.5rem', borderRadius: '10px', borderColor: 'black'}}type="email" id="email" placeholder="Fill in your email" value={email} onChange={handleChange}/>
             <div> Subcribe to the newsletter for marketing.
-            <input type="checkbox" id="email" name="email" value={checked} onChange={handleChecked}></input>
+            <input type="checkbox" id="emailCheckbox" name="email" checked={checked} onChange={handleChecked}></input>
             </div>
 
            <div>
